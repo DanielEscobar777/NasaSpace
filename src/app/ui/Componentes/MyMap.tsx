fab05f719ff253fb55780c8cf364bacca7f2ed88
Felix Sarmiento <thedarkomgf@gmail.com>
2024-10-06 12:11:16 -0400

    s

--- src/app/ui/Componentes/MyMap.tsx
+++ src/app/ui/Componentes/MyMap.tsx
@@ -17,7 +17,7 @@ import "leaflet/dist/leaflet.css";
 import "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/images/marker-icon.png";
 
-function MyMap({ opacity }) {
+function MyMap({ opacity }: { opacity: number }) {
   const [data, setData] = useState([]);
 
   const myIcon = L.icon({
