3b8ae746660e2987220d4c827158fb4669fea522
DanielEscobar777 <150990829+DanielEscobar777@users.noreply.github.com>
2024-10-07 14:58:19 -0400

    Update Slide5.tsx

--- src/app/ui/Slide5.tsx
+++ src/app/ui/Slide5.tsx
@@ -1,32 +1,17 @@
-import React from "react";
-import MapContainer from "./MapContainer";
+import React from 'react';
+
 const Slide5 = () => {
-  
   return (
-    <div
-      className="flex flex-col items-center p-4"
-      style={{ backgroundColor: "#f0f4f8", minHeight: "100vh" }}
-    >
-      <h2
-        className="text-2xl font-bold mb-4 text-center"
-        style={{ color: "#2980b9" }}
-      >
+    <div className="flex flex-col items-center p-0 bg-[#f0f4f8] min-h-screen w-full">
+      <h2 className="text-2xl font-bold mb-4 text-center text-[#2980b9] w-full">
         Información Sensores
       </h2>
-      <div style={{ width: "100%", maxWidth: "800px" }}>
-        <div
-          // src="https://dz7nzl1k-5173.brs.devtunnels.ms/"
+      <div className="w-full">
+        <iframe
+          src="https://dz7nzl1k-5173.brs.devtunnels.ms/"
           title="Contenido Interactivo"
-          style={{
-            width: "100%",
-            height: "600px",
-            border: "none",
-            borderRadius: "8px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <MapContainer />
-        </div>
+          className="w-full h-[calc(100vh-150px)] border-none rounded-lg shadow-md"
+        ></iframe>
       </div>
     </div>
   );
